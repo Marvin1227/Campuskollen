bd1c5c585f69f4b040cedde8a858f29114e338d6
Oscar Fyrk <oscar.fyrk@gmail.com>
2022-11-03 14:59:26 +0100

    torsdags fix
    sort och filter på search

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ function Home() {
     //test
     //const mt = g
 
-    console.log(groups[0].name);
+    //console.log(groups[0].name);
 
     const all = groups;
     
@@ -17,7 +17,12 @@ function Home() {
     return (
         <div className="Home">
             <Searchbar/>
-            <Knapp titel="Program" link="/legionen"/>
+            <div className="KnappWrapper1">
+                <Knapp titel="Program" link="/legionen"/>
+                <Knapp titel="Festerier" link="/legionen"/>
+                <Knapp titel="Phadderier" link="/legionen"/>
+                <Knapp titel="Föreningar" link="/legionen"/>
+            </div>
             <KnappProg titel="Medieteknik" link="/legionen"/>
             <p></p>
         </div>
--- src/searchbar.js
+++ src/searchbar.js
@@ -2,34 +2,37 @@ import { useState } from "react";
 import {groups} from './db';
 
 function Searchbar(props) {
+    
+    const sorted_groups = groups.sort((a,b) => a.name.localeCompare(b.name));
+    //console.log(sorted_groups);
+
     const [searchWord, setText] = useState("");
 
-    const onSearch= (searchTerm)=>{
-
+    const onSearch = (searchTerm)=>{
         console.log('search',searchTerm);
     }
 
     function setSearchString(event) {
-        setText(event.target.value);
+        setText(event.target.value); //uppdatera searchword
     }
-//Måste tolowercase 
-    function getName(name){
-        let tolowersearchword = toLowerCase(name);
-        let tolowerdb = toLowerCase(frat.name);
-        let foundName = groups.find(frat => tolowerdb === tolowersearchword);
-        return foundName;
+
+    function getName(group){
+        return group.name.toLowerCase().indexOf(searchWord.toLowerCase()) >= 0; //funktion för att filtrera på searchword
     }
-    const foundName = getName(searchWord);
-    console.log(foundName);
+    var filtered = sorted_groups.filter(getName); //filtrering
+    //if(searchWord){
+    //    filtered = sorted_groups.filter(getName); //filtrering
+    //    console.log(filtered);
+    //}
+    
 
     return ( 
-        
     <div className="Wrapper">
         <div onChange={()=> onSearch(searchWord)}>
             <input className="Searchbar" type="text" placeholder="Sök.." onChange={setSearchString} />
         </div> 
         <div className="Dropdown">
-        
+            {filtered.map((group) => (<div className="dropdown-row"> {group.name}</div>))}
         </div>
     </div>
     );
